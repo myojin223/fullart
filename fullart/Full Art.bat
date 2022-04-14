e18bffe36bd3b2ec09d3ed5169f12aed10fb4486
PedroLuisBernardos <65719334+PedroLuisBernardos@users.noreply.github.com>
2022-04-13 23:09:11 -0400

    change art_source and add flavor text

--- fullart/Full Art.bat
+++ fullart/Full Art.bat
@@ -1,1 +1,1 @@
-java -jar proximity.jar --cards=Decklist.txt --art_source=NONE --reminder_text=false --template=fullart --copyright=false --use_card_back=false --flavor_text=false --border=none
+java -jar proximity.jar --cards=Decklist.txt --art_source=BEST --reminder_text=false --template=fullart --copyright=false --use_card_back=false --border=none
